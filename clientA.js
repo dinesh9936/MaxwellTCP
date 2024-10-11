b8457180c20e8a5af19d82afd14b73c33b26a34a
Dinesh Kumar <dineshkumarcse0060@gmail.com>
2024-10-11 12:04:21 +0530

    hdksh

--- clientA.js
+++ clientA.js
@@ -1,7 +1,7 @@
 const net = require('net');
 const readline = require('readline');
 
-const meterClient = net.createConnection({ host: '2406:da1a:965:aa49:142f:830e:cc27:be3c', port: 8080 }, () => {
+const meterClient = net.createConnection({ host: 'https://e436-117-192-207-178.ngrok-free.app',port:4040}, () => {
     console.log('Meter connected to server.');
 });
 
@@ -18,4 +18,13 @@ meterClient.on('data', (data) => {
     if (received.includes('User sent:')) {
         console.log('Question received from User.');
         
-        // Prompt Meter to type a
+        // Prompt Meter to type a response
+        rl.question('Type your response: ', (response) => {
+            meterClient.write(response); // Send the typed response back to the server (and User)
+        });
+    }
+});
+
+meterClient.on('end', () => {
+    console.log('Disconnected from server.');
+});
